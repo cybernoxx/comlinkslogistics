24b89c6f2e93cbf48453c140be9d32b7f4514e27
Favoured Victor <turuthivictor9@gmail.com>
2018-07-11 11:30:00 +0300

    minify js

--- js/core.js
+++ js/core.js
@@ -1,8 +1,1 @@
-/*------- Page Loader -------*/
-
- if ((".loader").length) {
-      // show Preloader until the website ist loaded
-      $(window).on('load', function () {
-        $(".loader").fadeOut("slow");
-      });
-    }
+(function($){$(".navbar-nav li a").on('click',function(event){$('.navbar-nav li').removeClass('active');$(this).closest('li').addClass('active');var $anchor=$(this);var nav=$($anchor.attr('href'));if(nav.length){$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'easeInOutExpo');event.preventDefault()}});$(".navbar-collapse a").on('click',function(){$(".navbar-collapse.collapse").removeClass('in')});$("a.mouse-hover, a.get-quote").on('click',function(event){var hash=this.hash;if(hash){event.preventDefault();$('html, body').animate({scrollTop:$(hash).offset().top},1500,'easeInOutExpo')}})})(jQuery)
